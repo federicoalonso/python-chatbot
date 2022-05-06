9eb78e962bd67c860f44ef2fccd0fb358d734fed
Federico Alonso <f.nicolas.alonso@gmail.com>
2022-05-06 19:38:56 -0300

    model trained

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # python-chatbot
+
+## Introduction
+
+This repo is created to save a simple chatbot made in python with a basic neural network on it.
+
+The main porpoise is to learn how to use python and start learning the basics of neural networks.
+
+Video followed:
+
+* [Intelligent AI Chatbot in Python](https://www.youtube.com/watch?v=1lwddP0KUEg)
---
+++ training.py
@@ -0,0 +1,73 @@
+import random
+import numpy as np
+import json
+import pickle
+
+import nltk
+
+from nltk.stem import WordNetLemmatizer
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation
+from tensorflow.keras.optimizers import SGD
+
+lemmatizer = WordNetLemmatizer()
+
+intents = json.loads(open('intents.json').read())
+words = []
+classes = []
+documents = []
+ignore_letters = ['?', '!', '.', ',']
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        word_list = nltk.word_tokenize(pattern)
+        words.extend(word_list)
+        documents.append((word_list, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+# print(words)
+
+words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_letters]
+words = sorted(list(set(words)))
+
+# print(words)
+
+calsses = sorted(list(set(classes)))
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
+
+training = []
+output_empty = [0] * len(classes)
+
+for doc in documents:
+    bag = []
+    word_pattern = doc[0]
+    word_pattern = [lemmatizer.lemmatize(w.lower()) for w in word_pattern]
+    for w in words:
+        bag.append(1) if w in word_pattern else bag.append(0)
+    
+    output_row = list(output_empty) 
+    output_row[calsses.index(doc[1])] = 1
+    training.append([bag, output_row])
+
+random.shuffle(training)
+training = np.array(training)
+
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
+
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.model')
+print("Model Saved")
